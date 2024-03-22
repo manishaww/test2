cb3dddf66bccd6de8f3b3060c23d39a6497b3a7b
manishaww <162180175+manishaww@users.noreply.github.com>
2024-03-21 22:54:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/paneer_butter_masala")
+def paneer_butter_masala():
+    return render_template("paneer_butter_masala.html")
+
+@app.route("/jalebi")
+def jalebi():
+    return render_template("jalebi.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
